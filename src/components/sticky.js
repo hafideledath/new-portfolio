8971749c12052b2e85c5a32db7038c895a6f2720
Hafid Eledath <105419944+hafideledath@users.noreply.github.com>
2026-01-01 20:28:18 +0400

    added note for AO

--- src/components/sticky.js
+++ src/components/sticky.js
@@ -17,7 +17,7 @@ const Sticky = () => {
         <span>×</span> 
       </button>
       <p>Navigate by clicking through the files.</p><br /><br />
-      <p>You can contact me at hafideledath AT gmail DOT com.</p>
+      <p>You can contact me at hafideledath AT gmail DOT com.</p><br /><br />
       <p>Oh, and hello, admissions officer. Hope you're having a nice day.</p>
     </div>
   );
